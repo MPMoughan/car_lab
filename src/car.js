76038b0dd1cf119c64af68a8759fcecfdc1b8a35
MPMoughan <MattM@AmandoBs-MacBook-Pro-2.local>
2014-10-09 13:10:54 -0700

    finishing js functions - now onto testing

--- src/car.js
+++ src/car.js
@@ -1,13 +1,69 @@
-function Car(make, model, year, color){
-
+function Car(make, model, color){
+  this.make = make;
+  this.model = model;
+  this.color = color;
+  this.state = "off";
+  this.previous_owner = [];
+  this.current_owner = "manufacturer";
+  this.passengers = [];
 }
 
 Car.prototype.sale = function(newOwner){
-
+  this.previous_owners.push(this.current_owner);
+  this.current_owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
+  this.color = newColor;
+};
 
+Car.prototype.start = function(){
+  this.state = "on";
+};
+
+Car.prototype.off = function(){
+  this.state = "off";
+};
+
+Car.prototype.driveTo = function(destination){
+  if(this.state === "on"){
+  console.log("driving to " + destination);
+  return "driving to " + destination;
+  }
+};
+
+Car.prototype.park = function() {
+  if(this.state === "off"){
+    console.log("parked!");
+    return "parked!";
+  }
+};
+
+Car.prototype.pickUp = function(name){
+  if(this.state === "on"){
+  console.log("driving to pick up " + name);
+  this.passengers.push(name);
+  }
+};
+
+Car.prototype.dropOff = function(name){
+  if(this.state === "off" && this.passengers.indexOf(name) !== -1 ){
+    this.passengers.splice(this.passengers.indexOf(name),1);
+  }
 };
 
 module.exports=Car;
+
+// Implement the following methods:
+
+// Car#dropOff it should take a name and remove them from the passengers array,
+// but only if they are in the array.
+//It should also only drop them off if the car is on.
+
+
+
+
+
+
+
+
